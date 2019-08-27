dfca130036248c07f964d0ae1575e9ae90c28ce3
Cecilia Perez <cecy@girasolo.com>
2019-08-27 18:28:04 -0500

    added web site bigpink

---
+++ src/routes.js
@@ -0,0 +1,23 @@
+import React from 'react';
+//import {Route, Switch } from 'react-ro'
+
+import {Route, Switch} from 'react-router-dom';
+
+//componentes
+
+import App from './App';
+import About from './components/About.js';
+
+
+const AppRouters =() =>
+<App>
+<Switch>
+
+    <Route path="/about" component ={About} />
+</Switch>
+
+
+</App>
+
+
+export default AppRouters;
