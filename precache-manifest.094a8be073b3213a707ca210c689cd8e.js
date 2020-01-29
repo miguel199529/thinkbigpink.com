86a93e9b2211582b7ee8c8d8f6cb8b6e39cdadf3
Miguel Ángel López <miguel@girasolo.com>
2020-01-29 14:24:44 -0600

    Updates

---
+++ precache-manifest.094a8be073b3213a707ca210c689cd8e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58bae69a9c9a3a4da218391b2a272fdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55d3d863aaf41616682a",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "46fb3a5d89c1b4c429f1",
+    "url": "/static/css/main.63bf1f5b.chunk.css"
+  },
+  {
+    "revision": "55d3d863aaf41616682a",
+    "url": "/static/js/2.448c27ef.chunk.js"
+  },
+  {
+    "revision": "46fb3a5d89c1b4c429f1",
+    "url": "/static/js/main.4970a9f4.chunk.js"
+  },
+  {
+    "revision": "4289d1bc02841063a299",
+    "url": "/static/js/runtime~main.731e220e.js"
+  },
+  {
+    "revision": "4cf2472d9d30e5b2c500c5cd6198ca37",
+    "url": "/static/media/1.4cf2472d.png"
+  },
+  {
+    "revision": "fe902c77b94f515ecbba6be3e21c2144",
+    "url": "/static/media/2.fe902c77.png"
+  },
+  {
+    "revision": "d62c073b04458f314564a79bf3cd1102",
+    "url": "/static/media/4.d62c073b.png"
+  },
+  {
+    "revision": "6fdd3327187d359aa71182ef005eb344",
+    "url": "/static/media/5.6fdd3327.png"
+  },
+  {
+    "revision": "6c9c9f3341cc6375d01461408ba53462",
+    "url": "/static/media/Hack-Regular.6c9c9f33.ttf"
+  },
+  {
+    "revision": "47aec9c8bfc27ddbe47caa70801365a5",
+    "url": "/static/media/LogotipoGirasolo.47aec9c8.svg"
+  },
+  {
+    "revision": "31073ea4cb3e2b90ae778bfcc044628e",
+    "url": "/static/media/LogotipoOficial.31073ea4.svg"
+  },
+  {
+    "revision": "9e03f125a5d1e76caad42a18e6d90ad7",
+    "url": "/static/media/LogotipoOficial.9e03f125.svg"
+  },
+  {
+    "revision": "b9db9a0e68de14db22a93a4a25131713",
+    "url": "/static/media/OndasPaginaGirasolo.b9db9a0e.svg"
+  },
+  {
+    "revision": "e475fea4fec635a420f157423ee0d4ca",
+    "url": "/static/media/PacUeco.e475fea4.svg"
+  }
+]);
